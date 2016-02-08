601ca4309ddbf6342dfa1e4fcd1e0d76b58ac3a6
NewGyu <NewGyu@kinoboku.net>
2016-02-08 19:44:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# aws-ecs-z
+
+Wrapper of [AWS ECS SDK](http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/ECS.html)
+
+## usage
+
+ATDK
---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+module.exports.Service = require("./lib/Service");
+module.exports.TaskDefinition = require("./lib/TaskDefinition");
+module.exports.Task = require("./lib/Task");
